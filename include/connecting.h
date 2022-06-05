db43fd4a5287aad2ca5a1f78c2fdc32515db4221
bsquillari <bsquillari@itba.edu.ar>
2022-06-05 16:54:04 -0300

    add: connecting.c * Etapa de conexión * Actualmente se conecta a un destino fijo. * Cuando termina pasa al estado COPY.

---
+++ include/connecting.h
@@ -0,0 +1,31 @@
+#ifndef PROYECTO_PROTOS_CONNECTING_H
+#define PROYECTO_PROTOS_CONNECTING_H
+#include "selector.h"
+#include "debug.h"
+#include "socks5nio.h"
+#include "stm.h"
+#include "states.h"
+/**
+ * Etapa de conexión
+ * Actualmente se conecta a un destino fijo.
+ * Cuando termina pasa al estado COPY.
+ */
+/**
+ *
+ * @param state
+ * @param key
+ */
+void connecting_init(const unsigned state, struct selector_key *key);
+/**
+ *
+ * @param key
+ * @return
+ */
+unsigned connecting_write(struct selector_key *key);
+/**
+ *
+ * @param key
+ * @return
+ */
+unsigned connecting_read(struct selector_key *key);
+#endif //PROYECTO_PROTOS_CONNECTING_H
